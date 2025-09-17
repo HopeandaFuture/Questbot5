98a83af962b7c2c42b817a9eb3400b3eb323f1f6
HopeandaFuture <109474770+HopeandaFuture@users.noreply.github.com>
2025-09-17 18:47:12 -0500

    Add files via upload

---
+++ webserver (2).py
@@ -0,0 +1,17 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Discord bot ok"
+
+def run():
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Web server is starting on port {port}...")
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
